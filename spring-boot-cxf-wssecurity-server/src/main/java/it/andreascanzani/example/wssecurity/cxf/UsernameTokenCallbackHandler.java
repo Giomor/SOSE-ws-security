4b20edde04091db9fd107081d86c08bc4d4341d6
Giomor98 <giorgio.morico@student.univaq.it>
2023-04-17 15:24:23 +0200

    First commit

---
+++ spring-boot-cxf-wssecurity-server/src/main/java/it/andreascanzani/example/wssecurity/cxf/UsernameTokenCallbackHandler.java
@@ -0,0 +1,18 @@
+package it.andreascanzani.example.wssecurity.cxf;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class UsernameTokenCallbackHandler implements CallbackHandler {
+
+	public void handle(Callback[] callbacks) {
+		WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+		if (pc.getIdentifier().equals("admin")) {
+			pc.setPassword("pwd123");
+		}
+	}
+	
+}
+
